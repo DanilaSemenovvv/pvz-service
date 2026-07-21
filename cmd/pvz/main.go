b4b162fb47db6d4fb98007d37f7f8db805f4bd79
Danila Semenov <danilasemenovvv04@gmail.com>
2026-07-21 16:31:18 +0300

    добавить grpc-gateway

--- cmd/pvz/main.go
+++ cmd/pvz/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/DanilaSemenovvv/pvz/internal/mygrpc"
 	"github.com/DanilaSemenovvv/pvz/internal/service"
 	"github.com/DanilaSemenovvv/pvz/internal/storage"
 	desc "github.com/DanilaSemenovvv/pvz/pkg/api/pvz/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -31,13 +36,35 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
-
 	pvzGrpcHandler := mygrpc.NewServer(orderSrv)
-
 	desc.RegisterPVZServiceServer(grpcServer, pvzGrpcHandler)
+
 	go func() {
 		fmt.Println("gRPC сервер запущен на порту 50051...")
 		if err := grpcServer.Serve(listener); err != nil {
+			log.Fatalf("Ошибка работы сервера: %v", err)
+		}
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	gwMux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+	err = desc.RegisterPVZServiceHandlerFromEndpoint(ctx, gwMux, "localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("ошибка регистрации эндпоинтов: %v", err)
+	}
+
+	httpServer := &http.Server{Addr: ":8080", Handler: gwMux}
+
+	go func() {
+		fmt.Println("HTTP сервер запущен на порту 8080...")
+		err := httpServer.ListenAndServe()
+		if err == http.ErrServerClosed {
+			fmt.Println("Сервер закрыт")
+		} else {
 			log.Fatalf("Ошибка работы сервера: %v", err)
 		}
 	}()
@@ -50,8 +77,14 @@ func main() {
 
 	log.Println("Получен сигнал ОС, начинаем плавную остановку сервера...")
 
-	grpcServer.GracefulStop()
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
 
+	err = httpServer.Shutdown(shutdownCtx)
+	if err != nil {
+		log.Fatalf("Ошибка закрытия сервера: %v", err)
+	}
+	grpcServer.GracefulStop()
 	db.Close()
 
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/DanilaSemenovvv/pvz
 go 1.25.1
 
 require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.29.0
 	github.com/jackc/pgx/v5 v5.10.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20260414002931-afd174a4e478
 	google.golang.org/grpc v1.82.1
 	google.golang.org/protobuf v1.36.11
 )
